2ba1e7709e0cf04653156ee5a90c6db0c40c1f10
EmilIvanichkovv <emil.ivanichkov@gmail.com>
2022-01-20 23:56:01 +0200

    Add Login validation

---
+++ scripts/login.js
@@ -0,0 +1,116 @@
+var database = window.localStorage
+
+const loginButton = document.getElementById('login-submit')
+const loginEmail = document.getElementById('login-email')
+const loginPassword = document.getElementById('login-password')
+
+const errorEmptyEmail = 'PLEASE ENTER E-MAIL'
+const errorInvalidEmail = 'EMAIL IS NOT VALID'
+const errorNonRegisteredEmail = 'E-MAIL IS NOT REGISTERED YET'
+const errorIncorrectPassword = 'PASSWORD IS NOT CORRECT'
+
+loginEmail.addEventListener('change', event => {
+    event.preventDefault()
+    checkLoginEmail()
+})
+
+loginPassword.addEventListener('change', event => {
+    event.preventDefault()
+    checkLoginPassword()
+})
+
+loginButton.addEventListener('click', loginPlayer)
+
+function loginPlayer() {
+    event.preventDefault()
+
+    let correctEmail = checkLoginEmail()
+    let correctPassword = checkLoginPassword()
+    if(correctEmail && correctPassword){
+        window.location.href = "game.html"
+    }
+}
+
+function checkLoginEmail() {
+    let inputEmail = loginEmail.value.trim()
+    if(inputEmail == '') {
+        setError(loginEmail, errorEmptyEmail)
+        return false
+    }
+    if(validateEmail(inputEmail) === false){
+        setError(loginEmail, errorInvalidEmail)
+        return false
+    }
+    if(checkEmailExistence(inputEmail) === false){
+        setError(loginEmail, errorNonRegisteredEmail)
+        return false
+    }
+    else {
+        setValid(loginEmail)
+        return true
+    }
+}
+
+function  checkLoginPassword() {
+    const inputEmail = loginEmail.value.trim();
+    const inputPassword = loginPassword.value.trim();
+    if(!checkEmailPasswordMatch(inputEmail, inputPassword)){
+        setError(loginPassword, errorIncorrectPassword)
+        return false
+    }
+    else {
+        setValid(loginPassword)
+        return true
+    }
+}
+
+function checkEmailPasswordMatch(inputEmail, inputPassword) {
+    const user = JSON.parse(database.getItem(`${inputEmail}`));
+    if ((user[1] === inputEmail) && (user[2] === inputPassword)) {
+        return true;
+    }
+    return false;
+}
+
+function checkEmailExistence(inputEmail){
+    if (database.getItem(`${inputEmail}`) === null) {
+        return false; 
+    }
+    return true; 
+}
+
+function setError(item, message) {
+    const itemParent = item.parentElement
+    const errorItem = itemParent.querySelector('small')
+
+    errorItem.innerHTML = `${message}`
+
+    item.classList.add('error')
+    item.classList.remove('valid')
+    item.classList.remove('neonBoxes')
+}
+
+function setValid(item) {
+    const itemParent = item.parentElement
+    const errorItem = itemParent.querySelector('small')
+
+    errorItem.innerHTML = ``
+
+    item.classList.add('valid')
+    item.classList.remove('error')
+    item.classList.remove('neonBoxes')
+}
+
+function setDefault(item){
+    item.value = ''
+    item.classList.remove('valid')
+    item.classList.add('neonBoxes')
+}
+
+function validateEmail(input)
+{
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(input)){
+    return (true)
+  }
+    return (false)
+}
